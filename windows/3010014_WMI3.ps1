3e5f6e845edaf7ea64c29133eb16ae861800d35e
no2play <69761867+no2play@users.noreply.github.com>
2025-05-28 22:17:10 +0700

    Delete windows/3010014_WMI3.ps1

--- windows/3010014_WMI3.ps1
+++
@@ -1,5 +0,0 @@
-$remoteComputer = "<SCADA_IP>"
-$process = "notepad.exe"
-$cred = Get-Credential
-
-Invoke-CimMethod -ClassName Win32_Process -MethodName Create -Arguments @{CommandLine = $process} -ComputerName $remoteComputer -Credential $cred
