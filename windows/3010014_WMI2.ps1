2e4b00c3bd3f094a8c4bc1511a27201dabcc80b5
no2play <69761867+no2play@users.noreply.github.com>
2025-05-28 22:16:58 +0700

    Delete windows/3010014_WMI2.ps1

--- windows/3010014_WMI2.ps1
+++
@@ -1,8 +0,0 @@
-$remoteComputer = "<SCADA_IP>"
-$process = "calc.exe"
-$username = "Administrator"
-$password = "YourPassword"
-
-# wmic /node:<computer> /user:<user> /password:<pass> process call create "calc.exe"
-$wmicCmd = "wmic /node:$remoteComputer /user:$username /password:$password process call create `"$process`""
-Invoke-Expression $wmicCmd
